c8bb8c5e64db00194a4b04aed5aa046ee4635109
Mohammad Yoga Pratama <yogmhmmd17@gmail.com>
2022-12-06 09:20:37 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:projekuas/view/home.dart';
+import 'package:projekuas/view/login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: LoginScreen(),
+  ));
+}
