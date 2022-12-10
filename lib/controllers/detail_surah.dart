14cd54a930c87c2cbd46f912b86e83f414375869
Mohammad Yoga Pratama <yogmhmmd17@gmail.com>
2022-12-10 22:43:58 +0800

    Create detail_surah.dart
    update

---
+++ lib/controllers/detail_surah.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:Quran.me/models/detail_surah_model.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+class DetailController extends GetxController {
+  // Future<DetailSurah> getDetailSurah(String id) async {
+  //   Uri url = Uri.parse("https://api.quran.gading.dev/surah/$id");
+  //   var res = await http.get(url);
+
+  //   Map<String, dynamic> data = (json.decode(res.body) as Map<String, dynamic>);
+  //   return DetailSurah.fromJson(data);
+  // }
+}
