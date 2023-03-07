ad88257eb66e63e5968e871f5ea0951bbbcbba87
Mohammad Yoga Pratama <yogmhmmd17@gmail.com>
2023-03-07 23:22:44 +0800

    Create constant.dart
    constant untuk warna

---
+++ lib/constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/animation.dart';
+
+final Color primaryColor = Color(0xFF488396);
+final Color secondaryColor = Color(0xFFFFFFFF);
+
+List kumpulanWarna = [primaryColor, secondaryColor];
