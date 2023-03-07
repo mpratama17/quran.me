25b24a02470c8fbd75aa36b9a5069de2e85d4375
Mohammad Yoga Pratama <yogmhmmd17@gmail.com>
2023-03-07 23:23:46 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
+import 'package:Quran.me/view/home.dart';
+import 'package:Quran.me/view/login.dart';
+import 'package:Quran.me/view/splash.dart';
 import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'package:projekuas/view/home.dart';
-import 'package:projekuas/view/login.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: LoginScreen(),
+    home: HomeScreen(),
   ));
 }
